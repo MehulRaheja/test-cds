d393a5455ab38b4402acde27a46706d5b329ae89
Mehul Raheja <81683759+MehulRaheja@users.noreply.github.com>
2025-04-19 21:17:29 +0530

    hmm2

--- server.js
+++ server.js
@@ -50,62 +50,300 @@ app.get('/cds-services' , (req, res) => {
 
 app.get('/cds-services/transplant-order-create', (req, res) => {
   res.json( {
-    "cards": [
-      {
-        "summary": "Suggest transplant referral",
-        "detail": "The patient appears eligible for a transplant evaluation. Consider placing a referral order.",
-        "indicator": "info",
+  
+      "cards":[  
+       {  
+            "summary":"Example",
+            "indicator":"info",
+            "extension":{  
+               "com.epic.cdshooks.card.detail.content-type":"text/html"
+            },
+            "detail":"Another card to test suggestions",
         "source": {
-          "label": "Transplant Eligibility Service",
-          "url": "https://your-service.com"
-        },
-        "suggestions": [
-          {
-            "label": "Add transplant referral order",
-            "uuid": "transplant-referral-001",
-            "actions": [
-              {
-                "type": "create",
-                "description": "Create a draft referral order for transplant evaluation.",
-                "resource": {
-                  "resourceType": "ServiceRequest",
-                  "status": "draft",
-                  "intent": "order",
-                  "category": [
-                    {
-                      "coding": [
-                        {
-                          "system": "http://terminology.hl7.org/CodeSystem/service-category",
-                          "code": "REF",
-                          "display": "Referral"
-                        }
-                      ]
-                    }
-                  ],
-                  "code": {
-                    "coding": [
-                      {
-                        "system": "http://snomed.info/sct",
-                        "code": "430193006",
-                        "display": "Referral to transplant service"
-                      }
-                    ]
+         "label": "Clinical Inferences",
+         "url": "https://www.example.com/",
+         "icon": "file://example/CDSHooks/images/example.png",
+         "topic": {
+           "code": "BCSCard2",
+           "system": "card-system",
+           "display": "BCS Card 2"
+         }
+         },
+         "links": [
+         {
+           "label": "Github",
+           "url": "https://github.com",
+           "type": "absolute"
+         },
+         {
+           "label": "R4 SMART Example App",
+           "url": "https://example.com/EpicSMARTApp/Default.aspx?appFhirVersion=R4",
+           "type": "smart",
+           "appContext": "%FNAME%-%EXTENSION;74901%-420fe522-193c-11eb-9552-460231621f93~!@#$%^&*()-+{}[]|\\"
+         },
+         ],
+         "overrideReasons":[
+         {
+           "code":"patrefused",
+           "system":"http://example.org/cds-services/fhir/CodeSystem/override-reasons",
+           "display":"Patient refused"
+         },
+         {
+           "code":"seecomment",
+           "system":"http://example123.org/cds-services/fhir/CodeSystem/override-reasons",
+         }
+        ],
+              "suggestions":[
+          {  
+                  "label":"Arthritis",
+            "uuid": "cf72fe83-1eb9-410c-94aa-04ec98736388",
+                  "actions":[  
+                     {  
+                        "type":"create",
+              "description":"Arthritis",
+                        "resource": {
+               "resourceType": "Condition",
+               "category": [
+                 {
+                 "coding": [
+                   {
+                   "system": "http://terminology.hl7.org/CodeSystem/condition-category",
+                   "code": "encounter-diagnosis",
+                   "display": "Encounter diagnosis"								
+                   }
+                 ],
+                 "text": "Encounter diagnosis"
+                 }
+               ],
+               "code": {
+                 "coding": [
+                 {
+                   "system": "urn:com.epic.cdshooks.action.code.system.cms-hcc",
+                   "code": "40",
+                   "display": "Arthritis"
+                 }
+                 ],
+                 "text": "Stomach ache"
+               }
+               },
+                     }
+                  ]
+               },
+         {  
+                  "label":"Stroke",
+            "uuid": "12035ae1-5d60-4f58-b922-882140b98283",
+                  "actions":[  
+                     {  
+                        "type":"create",
+              "description":"Stroke",
+                        "resource": {
+               "resourceType": "Condition",
+               "category": [
+                 {
+                 "coding": [
+                   {
+                   "system": "http://terminology.hl7.org/CodeSystem/condition-category",
+                   "code": "encounter-diagnosis",
+                   "display": "Encounter diagnosis"								
+                   }
+                 ],
+                 "text": "Encounter diagnosis"
+                 }
+               ],
+               "code": {
+                 "coding": [
+                 {
+                   "system": "urn:com.epic.cdshooks.action.code.system.cms-hcc",
+                   "code": "100",
+                   "display": "Stroke"
+                 }
+                 ],
+                 "text": "Stroke"
+               }
+               },
+                     }
+                  ]
+               },
+         {  
+                  "label":"Stroke",
+            "uuid": "1a53ba14-a06b-4e82-bbb0-09ae01a75515",
+                  "actions":[  
+                     {  
+                        "type":"create",
+                        "description":"Stroke prognosis",
+                        "resource": {
+               "resourceType": "Condition",
+               "category": [
+                 {
+                 "coding": [
+                   {
+                   "system": "http://terminology.hl7.org/CodeSystem/condition-category",
+                   "code": "problem-list-item",
+                   "display": "Problem List"
+                   }
+                 ],
+                 "text": "Problem list"
+                 }
+               ],
+               "code": {
+                 "coding": [
+                 {
+                   "system": "urn:oid:2.16.840.1.113883.6.90",
+                   "code": "R10.9"
+                   
+                 },
+                 {
+                   "system": "urn:oid:2.16.840.1.113883.6.96",
+                   "code": "271681002"
+                   
+                 }
+                 ],
+                 "text": "Stomach ache"
+               }
+               },
+                     }
+                  ]
+               },
+         {
+         "label":"Diabetes",
+         "uuid": "85126ce5-b0a7-4a54-86f4-d7b52426cc58",
+               "actions":[  
+                  {  
+                     "type":"create",
+                     "description":"Diabetes Order Set from CDS Hooks",
+                     "resource": {
+             "resourceType": "ServiceRequest",
+             "status": "draft",
+             "intent": "proposal",
+             "category": [
+               {
+               "coding": [
+               {
+                 "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-category",
+                 "code": "outpatient",
+                 "display": "Outpatient"
+               }]
+             }],
+             "code": {
+               "coding": [
+                 {
+                 "system": "urn:com.epic.cdshooks.action.code.system.orderset-item",
+                 "code": "DIABETES"
+               }
+               ]
+             }
+             },
                   },
-                  "subject": {
-                    "reference": "Patient/{{context.patientId}}"
+               ]
+            },
+          {  
+           "label":"Test IP Medication Order",
+           "uuid": "b1e0575b-2381-4625-b687-2def804d7c79",
+           "actions":[  
+              {  
+               "type":"create",
+               "description":"Test Medication IP Order",
+               "resource": {
+               "resourceType": "MedicationRequest",
+               "status": "draft",
+               "intent": "proposal",
+               "category": [
+               {
+                 "coding": [
+                 {
+                   "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-category",
+                   "code": "inpatient",
+                   "display": "Inpatient"
+                 }
+                 ],
+                 "text": "Inpatient"
+               }
+               ],
+               "medicationCodeableConcept": {
+                 "coding": [
+                 {
+                   "system": "urn:oid:2.16.840.1.113883.6.69",
+                   "code": "55111-682-01"
+                 }
+                 ],
+                 "text": "Test Med Display name"
+               }
+               },
+              },
+           ]
+          },
+          {  
+           "label":"Test Medication Order from Pref",
+           "uuid": "5e0ce5e2-d33a-467c-bfc6-cde8d304e73d",
+           "actions":[  
+              {  
+               "type":"create",
+               "description":"Test Medication Order from Pref",
+               "resource": {
+               "resourceType": "MedicationRequest",
+               "status": "draft",
+               "intent": "proposal",
+               "category": [ 
+               {
+                 "coding": [
+                 {
+                   "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-category",
+                   "code": "inpatient",
+                   "display": "Inpatient"
+                 }
+                 ],
+                 "text": "Inpatient"
+               }
+               ],
+               "medicationCodeableConcept": {
+                 "coding": [
+                 {
+                   "system": "urn:com.epic.cdshooks.action.code.system.preference-list-item",
+                   "code": "BENAD25"
+                 }
+                 ],
+                 "text": "Test Proc Display name"
+               }
+               },
+              },
+           ]
+          },
+        {  
+               "label":"CBC",
+         "uuid": "613b0192-4243-4384-8294-4316dfb726bb",
+               "actions":[  
+                  {  
+                     "type":"create",
+                     "description":"CBC from CDS Hooks",
+                     "resource": {
+             "resourceType": "ServiceRequest",
+             "status": "draft",
+             "intent": "proposal",
+             "category": [
+               {
+               "coding": [
+               {
+                 "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-category",
+                 "code": "outpatient",
+                 "display": "Outpatient"
+               }]
+             }],
+             "code": {
+               "coding": [
+                 {
+                 "system": "urn:com.epic.cdshooks.action.code.system.preference-list-item",
+                 "code": "CBC_IP"
+               }
+               ],
+               "text": "Test Proc Display name"
+             }
+             },
                   },
-                  "authoredOn": "{{context.timestamp}}",
-                  "requester": {
-                    "reference": "Practitioner/{{context.userId}}"
-                  },
-                  "priority": "routine"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ]
+               ]}            
+       ]
+         },
+   ]
+   
   })
 })
 app.post('/cds-services/transplant-order-create', (req, res) => {
